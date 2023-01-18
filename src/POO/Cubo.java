f7042ea59759e513b8eb4865aa2ad890cbfd0e29
Wally <jesusdediossanchez@gmail.com>
2023-01-18 19:27:57 +0100

    Commit subida de archivos
    He subido el archivo cubo y el main actualizados

--- src/POO/Cubo.java
+++ src/POO/Cubo.java
@@ -1,5 +1,7 @@
 package POO;
 
+import javax.swing.JOptionPane;
+
 public class Cubo {
 	private String material;
 	private boolean asa;
@@ -7,10 +9,108 @@ public class Cubo {
 	private double capacidad;
 	private double capacidadActual;
 	public Cubo(){
-		this.material="caucho";
+		this.material="plástico";
 		this.asa=true;
 		this.color="azul";
 		this.capacidad=10;
 		this.capacidadActual=0;
 	}
-}
+	public Cubo(String material,boolean asa,String color,double capacidad,double capacidadActual){
+		this.material=material;
+		this.asa=asa;
+		this.color=color;
+		this.capacidad=capacidad;
+		this.capacidadActual=capacidadActual;
+	}
+	/**
+	 * @return the material
+	 */
+	public String getMaterial() {
+		return material;
+	}
+	/**
+	 * @return the asa
+	 */
+	public boolean isAsa() {
+		return asa;
+	}
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+	/**
+	 * @return the capacidad
+	 */
+	public double getCapacidad() {
+		return capacidad;
+	}
+	/**
+	 * @return the capacidadActual
+	 */
+	public double getCapacidadActual() {
+		return capacidadActual;
+	}
+	/**
+	 * @param material the material to set
+	 */
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	/**
+	 * @param asa the asa to set
+	 */
+	public void setAsa(boolean asa) {
+		this.asa = asa;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+	/**
+	 * @param capacidad the capacidad to set
+	 */
+	public void setCapacidad(double capacidad) {
+		this.capacidad = capacidad;
+	}
+	/**
+	 * @param capacidadActual the capacidadActual to set
+	 */
+	public void setCapacidadActual(double capacidadActual) {
+		this.capacidadActual = capacidadActual;
+	}
+	public void llenarCubo(double numeroLitros) {
+		do {
+			
+
+		if (numeroLitros>=0 && numeroLitros<10) {
+			this.capacidadActual+=numeroLitros;
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Introduzca cifra entre 0 y 10");
+		}
+		}while(numeroLitros<10);
+		
+	}
+	public  void vaciarCubo(double numeroLitros) {
+		do {
+			if (numeroLitros>=0 && numeroLitros<10) {
+				this.capacidadActual-=numeroLitros;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Introduzca cifra entre 0 y 10");
+			}
+			}while(numeroLitros<10);
+			
+	}
+	public void pintarCubo(double numeroLitros) {
+		if(numeroLitros>0 && numeroLitros<2) {
+			JOptionPane.showMessageDialog(null, color);
+		}
+	}
+	}
+
--- src/POO/Main.java
+++ src/POO/Main.java
@@ -1,11 +1,19 @@
 package POO;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
+		double numeroLitros;
+
 		Cubo cuboPorDefecto=new Cubo();
-		System.out.println(cuboPorDefecto);
-		
+		Cubo cuboConParametro=new Cubo("plástico", false, "negro", 10, 0);
+		numeroLitros=Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos litros quieres llenar?"));
+		cuboPorDefecto.llenarCubo(numeroLitros);
+		numeroLitros=Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos litros quieres vaciar?"));
+		cuboPorDefecto.vaciarCubo(numeroLitros);
+		cuboPorDefecto.setCapacidadActual(numeroLitros);
 	}
 
 }
