0a6a357289511c3824ffb14fe005b4065fb3b4c9
Jesús De Dios.R4 <Jess De Dios.R4@DESKTOP-5FAD0MS>
2023-01-23 21:45:40 +0100

    commit triangulo
    Subido clase ejercicio1

---
+++ src/POO/Triangulo.java
@@ -0,0 +1,27 @@
+package POO;
+
+public class Triangulo {
+	private double base;
+	private double altura;
+	/**
+	 * @param base
+	 * @param altura
+	 */
+	public Triangulo() {
+		this.base = base;
+		this.altura = altura;
+	}
+	public double getBase() {
+		return base;
+	}
+	public void setBase(double base) {
+		this.base = base;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+}
