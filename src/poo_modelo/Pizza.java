01b3adead4c72c278da3c9c13ae5ee88562ed49b
Jdds97 <jesusdediossanchez@gmail.com>
2023-02-20 21:45:48 +0100

    Terminar pizzeria

---
+++ src/poo_modelo/Pizza.java
@@ -0,0 +1,11 @@
+package poo_modelo;
+
+public class Pizza extends Pizzeria {
+	private enum tamaño{mediana,familiar};
+	private enum tipo{margarita,cuatroQuesos,funghi};
+	public Pizza() {
+		
+		
+	}
+
+}
---
+++ src/poo_modelo/Pizzeria.java
@@ -0,0 +1,9 @@
+package poo_modelo;
+
+public abstract class Pizzeria {
+
+	public Pizzeria() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
