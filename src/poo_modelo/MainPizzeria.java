6b7ab1dd1dac534e3d9a37b56feba41a3af19429
Jdds97 <jesusdediossanchez@gmail.com>
2023-02-22 21:31:03 +0100

    subida MainPizzeria

---
+++ src/poo_modelo/MainPizzeria.java
@@ -0,0 +1,29 @@
+package poo_modelo;
+
+import javax.swing.JOptionPane;
+import poo_modelo.Pizzeria;
+import poo_modelo.Pizza;
+import poo_modelo.Pizza.Tamaño;
+import poo_modelo.Pizza.Tipo;
+
+public class MainPizzeria {
+
+
+
+	public static void main(String[] args) {
+		Pizza margarita=new Pizza(Tipo.margarita, Tamaño.familiar);
+		Pizza cuatroQuesos=new Pizza(Tipo.cuatroQuesos, Tamaño.mediana);
+		Pizza funghi=new Pizza(Tipo.funghi, Tamaño.familiar);
+		JOptionPane.showMessageDialog(null,margarita.tamañoPizza);
+		JOptionPane.showMessageDialog(null,cuatroQuesos.tamañoPizza);
+		JOptionPane.showMessageDialog(null,funghi.tamañoPizza);
+		JOptionPane.showMessageDialog(null,margarita.tipoPizza);
+		JOptionPane.showMessageDialog(null,cuatroQuesos.tipoPizza);
+		JOptionPane.showMessageDialog(null,funghi.tipoPizza);
+		
+    Paninis paninis=new Paninis();
+	paninis.mostrarPaninis();
+        
+}
+	}
+	}
