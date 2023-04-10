6b95c1b65e048963ff4f395ad6d8bdd523506bd4
Jdds97 <jesusdediossanchez@gmail.com>
2023-04-10 12:18:42 +0200

    Terminado ejercicio 2

--- src/colecciones/Ejercicio2.java
+++ src/colecciones/Ejercicio2.java
@@ -6,16 +6,20 @@ import java.util.Random;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
+		//Creamos el numero aleatorio
 		Random rand = new Random();
-		int numeroAleatorioArray = 20;
-
+		int numeroAleatorioArray =10 + rand.nextInt(11);
+		//Creamos la lista con un numero aleatorio 
 		ArrayList<Integer> numerosAleatorios = new ArrayList<>(numeroAleatorioArray);
-
-		for (int i=0;i<numerosAleatorios.size();i++) {
-			int numero=rand.nextInt(100);
-			numerosAleatorios.add(numero);
+		//Recorremos la array con el número aleatorio que hemos pasado
+		//al no tener valores añadidos la longitud del array está vacía 
+		for (int i=0;i<numeroAleatorioArray;i++) {
+			//Añadimos por cada iteracción del numero aleatorio un número aleatorio
+			//entre 0 y 100
+			numerosAleatorios.add(rand.nextInt(100));
 		}
-		System.out.println(numerosAleatorios.size());
+		//Mostramos por pantalla los numeros aleatorios creados en la lista
+		System.out.println(numerosAleatorios);
 	}
 
 }
